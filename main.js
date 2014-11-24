3256411c8cd95646580afd3b5f7ab479f6c67961
anniemcghee <mcghee.annie@gmail.com>
2014-11-23 18:51:30 -0800

    TICTACDONE

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 document.addEventListener('DOMContentLoaded', function(){
-	var clearBoardButton = document.querySelector('#refresh');
+	var clearBoardButton = document.getElementById('refresh');
 	var allButtons = document.querySelectorAll('.btn-md'); 
 	var turnCounter = 0;
+	var winningArray = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[6,4,2]];
+
+	function checkForWinner() {
+		for (i = 0; i < winningArray.length; i++) {
+			if (allButtons[winningArray[i][0]].innerText !== "-" &&
+				allButtons[winningArray[i][1]].innerText === allButtons[winningArray[i][0]].innerText && 
+				allButtons[winningArray[i][2]].innerText === allButtons[winningArray[i][0]].innerText) {
+				alert(allButtons[winningArray[i][1]].innerText + " has won!");
+				return allButtons[winningArray[i][1]].innerText;
+			};
+		}
+	}
+
+	clearBoardButton.addEventListener('click', function () {
+		for (i=0; i <allButtons.length; i++) {
+			allButtons[i].innerText = "-";
+			allButtons[i].disabled = false;
+		}
+	});
 	
 	for (i = 0; i < allButtons.length; i++) {
 		allButtons[i].addEventListener('click', function () {
 			turnCounter++;
 			if (turnCounter %2 === 1) {
-				this.innerText = "X";
+				this.innerText = "X"; 
+				this.disabled = true;
 			}
 			else {
 				this.innerText = "O";
+				this.disabled = true;
 			}
+			checkForWinner();
 		});
 	}
 
+});
 	// allButtons[0].addEventListener('click', function() {
 	// 	turnCounter++;
 	// 	if (turnCounter%2 === 1) {	
@@ -98,5 +121,5 @@ document.addEventListener('DOMContentLoaded', function(){
 	// });
 
 
-});
+
 
