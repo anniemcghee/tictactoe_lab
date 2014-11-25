5cadef46dedea1553a58f660362214ae78f5e485
anniemcghee <mcghee.annie@gmail.com>
2014-11-25 15:34:08 -0800

    More jQuery animation

--- jquery_version/main2.js
+++ jquery_version/main2.js
@@ -3,32 +3,45 @@ $(function() {
 	var allButtons = $('.btn-md');
 	var turnCounter = 0;
 
-	// $(".page-header").on ("click", function() {
-	// 	$('.btn-md').fadeIn("slow", function(){
-	// 	});
-	// //animation complete
-	// });
-
 	allButtons.on('click', function () {
-
+		$(this)
+			.animate({
+				width:"110px",
+				height:"110px",
+				}, 200)
+			.animate({
+				width:"100px",
+				height:"100px",
+			}, 200, function(){
+			checkForWinner();
+			});
+		//animation complete
 		turnCounter++;
 		if (turnCounter %2 === 1) {
-			$(this).text("X"); 
+		$(this).text("X"); 
 		}
 		else {
-			$(this).text("O");
+		$(this).text("O");
 		}
 		$(this).attr('disabled', true);
-		checkForWinner();
-		});
+	});
 
 	clearBoardButton.on('click', function() {
+		$(this)
+			.animate({
+				width:"60px",
+				height:"55px",
+				}, 200)
+			.animate({
+				width:"50px",
+				height:"45px",
+			}, 200)
 		$(allButtons).text('-');
 		$(allButtons).attr('disabled', false);
 	});
 
 	var checkForWinner = function () {
-		
+
 		var winningArray = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[6,4,2]];
 
 		for (i = 0; i < winningArray.length; i++) {
@@ -36,9 +49,21 @@ $(function() {
 				allButtons[winningArray[i][1]].innerText === allButtons[winningArray[i][0]].innerText && 
 				allButtons[winningArray[i][2]].innerText === allButtons[winningArray[i][0]].innerText) {
 				$(allButtons).attr('disabled', true);
-				alert(allButtons[winningArray[i][1]].innerText + " has won!");
+			alert(allButtons[winningArray[i][1]].innerText + " has won!");
+				$(allButtons)
+				.animate({
+					width:"115px",
+					height:"115px",
+					}, 450)
+				.animate({
+					width:"100px",
+					height:"100px",
+				}, 450)
 			return allButtons[winningArray[i][1]].innerText;
-		};
+			};
+		}
 	}
-}
 });
+
+
+
