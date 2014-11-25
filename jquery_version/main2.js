ad6b9a9fe1dd5db5c9dc07d7f568484d6c309ca6
anniemcghee <mcghee.annie@gmail.com>
2014-11-24 17:29:02 -0800

    JQuery Update

--- jquery_version/main2.js
+++ jquery_version/main2.js
@@ -6,16 +6,31 @@ $(function() {
 	allButtons.on('click', function () {
 		turnCounter++;
 		if (turnCounter %2 === 1) {
-				$(this).text("X"); 
-			}
-			else {
-				$(this).text("O");
-			}
+			$(this).text("X"); 
+		}
+		else {
+			$(this).text("O");
+		}
 		$(this).attr('disabled', true);
+		checkForWinner();
 	});
 
 	clearBoardButton.on('click', function() {
 		$(allButtons).text('-');
 		$(allButtons).attr('disabled', false);
 	});
+
+	var checkForWinner = function () {
+		
+		var winningArray = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[6,4,2]];
+
+		for (i = 0; i < winningArray.length; i++) {
+			if (allButtons[winningArray[i][0]].innerText !== "-" &&
+				allButtons[winningArray[i][1]].innerText === allButtons[winningArray[i][0]].innerText && 
+				allButtons[winningArray[i][2]].innerText === allButtons[winningArray[i][0]].innerText) {
+				alert(allButtons[winningArray[i][1]].innerText + " has won!");
+			return allButtons[winningArray[i][1]].innerText;
+		};
+	}
+}
 });
